e1bf9ae9e34d5744bbf99cef30cc20a8037c0096
jinsb1995 <jinsb1995@gmail.com>
2022-10-25 14:35:25 +0900

    CI/CD 설정 완료 및 배포 완료 후 jar파일 실행

---
+++ scripts/deploy.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+REPOSITORY=/home/ec2-user/app/step2
+PROJECT_NAME=freelec-springboot2-webservice
+
+echo "> Build 파일 복사"
+cp REPOSITORY/zip/*.jar $REPOSITORY
+
+
+echo "> 현재 구동 중인 애플리케이션 pid 확인"
+CURRENT_PID=$(pgrep -fl freelec-springboot2-webservice | grep jar | awk '{print $1}')
+
+
+echo "현재 구동중인 애플리케이션 pid : $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+
+echo "> 새 애플리케이션 배포"
+
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+echo "> JAR Name : $JAR_NAME"
+
+echo "> $JAR_NAME에 실행 권한 추가"
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+
+
+nohup java -jar \
+ -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties,classpath:/application-real.properties \
+ -Dspring.profiles.active=real \
+ $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+
+
+# step1에서 했던 방식과 크게 다르지 않지만,
+# git pull을 이용해서 직접 빌드했던 부분이 제거되었다.
+
+# 그리고 jar를 실행하는 단계에서 몇가지 코드가 추가되었다.
